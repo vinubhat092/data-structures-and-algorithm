654d2010b17346c2a6f71bf8be0883515325f52e
vinubhat092 <vinayakbhat94@gmail.com>
2023-04-18 23:50:54 +0530

    ll2

--- linked_list.py
+++ linked_list.py
@@ -8,10 +8,62 @@ class LinkedList:
         self.head = None
 
     def insert_at_beginning(self,data):
-        node = Node(data, self.head)
-        self.head = node
+        node = Node(data, self.head)        #making new node and setting self.head as next
+        self.head = node    # making new node as self. head
 
-    def print(self):
+    def insert_at_end(self,data):
+        if self.head is None:
+            self.head = Node(data, None)
+            return
+        itr = self.head
+        while itr.next:    # till there are elements in the node list
+            itr = itr.next
+        itr.next = Node(data,None)    #creating a new node at the end 
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
+
+    def get_length(self):
+        count = 0
+        itr = self.head
+        while itr:
+            count +=1
+            itr = itr.next
+        return count
+    
+    def remove_at(self, index):
+        if index <0 or index > ll.get_length():
+            raise Exception("not possible")
+        if index == 0:
+            self.head = self.head.next   #if removing element is first node, make the second node as first node
+
+        itr = self.head
+        count = 0
+        while itr:
+            if count == index-1:    # if index is the node to be deleted , take the node previus to it and create the connection to the next node of the node which is getting deleted
+                itr.next = itr.next.next
+                break
+            itr = itr.next
+            count +=1
+    def insert_at(self,index,data):
+        if index <0 or index > ll.get_length():
+            raise Exception("not possible")
+        if index == 0:
+            self.insert_at_beginning(data)
+            return
+        itr = self.head
+        count = 0
+        while itr:
+            if count == index -1:
+                node = Node(data,itr.next)
+                itr.next = node
+                break
+            itr = itr.next
+            count = count+1
+
+    def print(self):           #utility function to print the output
         if self.head is None:
             print("linked list is empty")
             return
@@ -25,8 +77,9 @@ class LinkedList:
 
 if __name__ == "__main__":
     ll = LinkedList()
-    ll.insert_at_beginning(2)
-    ll.insert_at_beginning(3)
+    ll.insert_values(['banana','mango',"grapes",'orange'])
     ll.print()
-
+    
+    ll.insert_at(0,"figs")
+    ll.print()
          
